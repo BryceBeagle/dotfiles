bc3033b6a4865d29745e10144332e59aabf5c645
brycebeagle <bryce.beagle@gmail.com>
2018-08-05 10:13:22 -0700

    Link path now ensures that it has a filename

--- util.py
+++ util.py
@@ -10,6 +10,12 @@ def remove(path):
 
 
 def symlink(real, link):
+
+    # Add name of real file to link if link is merely a directory
+    if os.path.isdir(link):
+        link = os.path.join(link, os.path.basename(real))
+
+    # Expand potentially real path to be absolute
     real = os.path.abspath(real)
 
     # Create directory link will reside in if it does not exist
