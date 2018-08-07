a2b6f621a53c70091f98f5d2b067b78cf799964b
brycebeagle <bryce.beagle@gmail.com>
2018-08-06 23:24:59 -0700

    Use US mirrors

--- install.py
+++ install.py
@@ -53,7 +53,7 @@ def select_mirrors():
     #           "--sort", "rate",
     #           "--save", "/etc/pacman.d/mirrorlist"])
 
-    util.run(["curl -s 'https://www.archlinux.org/mirrorlist/?country=FR&country=GB&protocol=https&use_mirror_status=on' | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 5 - > /etc/pacman.d/mirrorlist"])
+    util.run(["curl -s 'https://www.archlinux.org/mirrorlist/?country=US&protocol=https&use_mirror_status=on' | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 5 - > /etc/pacman.d/mirrorlist"])
 
 
 def setup_localization():
