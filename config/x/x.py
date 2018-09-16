58e3ee4bb85c113d4b4cc40d9d1403d6c77de9bc
brycebeagle <bryce.beagle@gmail.com>
2018-09-16 15:33:16 -0700

    Add trailing slash to indicate that it's a directory

--- config/x/x.py
+++ config/x/x.py
@@ -2,5 +2,5 @@ import util
 
 
 def setup(username):
-    print(f"Symlinking .xinitrc to /home/{username}")
+    print(f"Symlinking .xinitrc to /home/{username}/")
     util.symlink(".xinitrc", f"/home/{username}/")
