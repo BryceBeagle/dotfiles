6775f2e4cb3538cb15e0b8d04fac646b77caaa6d
brycebeagle <bryce.beagle@gmail.com>
2018-09-25 00:31:58 -0700

    Actually run qtile setup

--- config/config.py
+++ config/config.py
@@ -2,6 +2,7 @@ from . import bash
 from . import fonts
 from . import nvim
 from . import ssh
+from . import qtile
 from . import x
 from . import xdg
 from . import zsh
@@ -9,6 +10,7 @@ from . import zsh
 
 def setup():
     x.setup()
+    qtile.setup()
     nvim.setup()
     xdg.setup()
     bash.setup()
