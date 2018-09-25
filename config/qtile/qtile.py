ec63982d3b94fac4beb2e50eb73bbb589297f559
brycebeagle <bryce.beagle@gmail.com>
2018-09-25 00:24:21 -0700

    Setup qtile properly

--- config/qtile/qtile.py
+++ config/qtile/qtile.py
@@ -1,2 +1,13 @@
+import util
+
+
 def setup():
-    ...
+
+    # Symlink all qtile files
+    util.symlink("qtile/config.py", "~/.config/qtile/")
+    util.symlink("qtile/bindings.py", "~/.config/qtile/")
+    util.symlink("qtile/groups.py", "~/.config/qtile/")
+    util.symlink("qtile/helpers.py", "~/.config/qtile/")
+    util.symlink("qtile/layouts.py", "~/.config/qtile/")
+    util.symlink("qtile/settings.py", "~/.config/qtile/")
+    util.symlink("qtile/widgets.py", "~/.config/qtile/")
