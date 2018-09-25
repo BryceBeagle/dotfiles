e7ca018184ed8a76a307ea87f0be641a63f0aea4
brycebeagle <bryce.beagle@gmail.com>
2018-09-25 00:34:46 -0700

    Fix qtile symlink target paths

--- config/qtile/qtile.py
+++ config/qtile/qtile.py
@@ -4,10 +4,10 @@ import util
 def setup():
 
     print("Symlink all qtile files")
-    util.symlink("qtile/config.py", "~/.config/qtile/")
-    util.symlink("qtile/bindings.py", "~/.config/qtile/")
-    util.symlink("qtile/groups.py", "~/.config/qtile/")
-    util.symlink("qtile/helpers.py", "~/.config/qtile/")
-    util.symlink("qtile/layouts.py", "~/.config/qtile/")
-    util.symlink("qtile/settings.py", "~/.config/qtile/")
-    util.symlink("qtile/widgets.py", "~/.config/qtile/")
+    util.symlink("config/qtile/config.py", "~/.config/qtile/")
+    util.symlink("config/qtile/bindings.py", "~/.config/qtile/")
+    util.symlink("config/qtile/groups.py", "~/.config/qtile/")
+    util.symlink("config/qtile/helpers.py", "~/.config/qtile/")
+    util.symlink("config/qtile/layouts.py", "~/.config/qtile/")
+    util.symlink("config/qtile/settings.py", "~/.config/qtile/")
+    util.symlink("config/qtile/widgets.py", "~/.config/qtile/")
