eb2aad07686317e56c861ca2a7d3328e2f180c24
brycebeagle <bryce.beagle@gmail.com>
2018-09-27 22:16:13 -0700

    Start working on a VolumeWidget that can actually use mouse buttons

--- config/qtile/widgets.py
+++ config/qtile/widgets.py
@@ -1,58 +1,69 @@
-import os
 import subprocess
 
-from settings import SCRIPT_DIR
-
-from libqtile.widget import base
+from libqtile.widget import volume
 
 
-class ShellScript(base.ThreadedPollText):
-    '''
-    A generic text widget that polls using a poll function to get its text
-    and accepts mouse interaction through the bar.
-    When clicked, the script will be able to pull out the following env vars:
-        WIDGET_BUTTON - (see below)
-        WIDGET_X_LOC  - X location in pixels
-        WIDGET_Y_LOC  - Y location in pixels
+class UsefulVolumeWidget(volume.Volume):
 
-    Mouse interactions come through as ints mapped as follows
-        1: LEFT
-        2: RIGHT
-        3: MIDDLE
-        4: SCROLL_UP
-        5: SCROLL_DOWN
-    '''
-    orientations = base.ORIENTATION_HORIZONTAL
-    defaults = [
-        ('fname', None, 'Filename in script directory'),
-        ('script_dir', SCRIPT_DIR, 'Directory containing the script'),
-    ]
+    def __init__(
+            self, *,
+            button_up=volume.BUTTON_UP,
+            button_down=volume.BUTTON_DOWN,
+            button_mute=volume.BUTTON_MUTE,
+            button_right=volume.BUTTON_RIGHT,
+            **kwargs):
+        super().__init__(**kwargs)
 
-    def __init__(self, **config):
-        base.ThreadedPollText.__init__(self, **config)
-        self.add_defaults(ShellScript.defaults)
-        self.fname = self.script_dir + config['fname']
-
-    def poll(self):
-        '''When polled just run the script without click info'''
-        return self._run_script()
-
-    def _run_script(self, btn=None, x=None, y=None):
-        '''Run the script, optionally passing click info in the environment'''
-        if btn is not None:
-            btn_env = os.environ.copy()
-            btn_env['WIDGET_BUTTON'] = str(btn)
-            btn_env['WIDGET_X_LOC'] = str(x)
-            btn_env['WIDGET_Y_LOC'] = str(y)
-            result = subprocess.run(
-                self.fname, stdout=subprocess.PIPE, env=btn_env)
-        else:
-            result = subprocess.run(self.fname, stdout=subprocess.PIPE)
-
-        return result.stdout.decode()
+        self.button_up = button_up
+        self.button_down = button_down
+        self.button_mute = button_mute
+        self.button_right = button_right
 
     def button_press(self, x, y, button):
-        '''
-        Pass the information off to the script but ignore the script output
-        '''
-        return self._run_script(btn=button, x=x, y=y)
+        if button == self.button_down:
+            if self.volume_down_command is not None:
+                subprocess.call(self.volume_down_command)
+            else:
+                subprocess.call(self.create_amixer_command('-q',
+                                                           'sset',
+                                                           self.channel,
+                                                           '%d%%-' % self.step))
+        elif button == self.button_up:
+            if self.volume_up_command is not None:
+                subprocess.call(self.volume_up_command)
+            else:
+                subprocess.call(self.create_amixer_command('-q',
+                                                           'sset',
+                                                           self.channel,
+                                                           '%d%%+' % self.step))
+        elif button == self.button_mute:
+            if self.mute_command is not None:
+                subprocess.call(self.mute_command)
+            else:
+                subprocess.call(self.create_amixer_command('-q',
+                                                           'sset',
+                                                           self.channel,
+                                                           'toggle'))
+        elif button == self.button_right:
+            if self.volume_app is not None:
+                subprocess.Popen(self.volume_app)
+
+        self.draw()
+
+    def cmd_increase_vol(self):
+        # Emulate button press.
+        self.button_press(0, 0, self.button_up)
+
+    def cmd_decrease_vol(self):
+        # Emulate button press.
+        self.button_press(0, 0, self.button_down)
+
+    def cmd_mute(self):
+        # Emulate button press.
+        self.button_press(0, 0, self.button_mute)
+
+    def cmd_run_app(self):
+        # Emulate button press.
+        self.button_press(0, 0, self.button_right)
+
+
